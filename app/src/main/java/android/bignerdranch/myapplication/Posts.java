b415fda51ae1fa2e3727453f41fd1183060e2637
Edmund-Liz <1836491284@qq.com>
2023-01-03 16:58:51 +0800

    初始界面 登录界面 单个帖子

--- app/src/main/java/android/bignerdranch/myapplication/Posts.java
+++ app/src/main/java/android/bignerdranch/myapplication/Posts.java
@@ -3,30 +3,17 @@ package android.bignerdranch.myapplication;
 import java.util.Date;
 
 public class Posts {
-<<<<<<< HEAD
-    //帖子类
-    private String name;//发布者的名字
-    private boolean follow;//你是否关注发布者
-    private String content;//帖子的文本内容
-    private Date ReleaseTime;//帖子的发布时间（可能不是用String）
-=======
     private String name;//发布者的名字
     private boolean follow;//是否关注发布者
     private String content;//发布内容
-    private Date releaseTime;
->>>>>>> liz
+    private Date releaseTime;//发布时间
+
 
     //构造器
     public Posts(String name, boolean follow, String content, Date releaseTime) {
         this.name = name;
         this.follow = follow;
         this.content = content;
-<<<<<<< HEAD
-        ReleaseTime = releaseTime;
-    }
-
-    //以下一长段都是自动生成的get和set方法
-=======
         this.releaseTime = releaseTime;
     }
 
@@ -34,56 +21,28 @@ public class Posts {
     public boolean isFollow() {
         return follow;
     }
-
     public void setFollow(boolean follow) {
         this.follow = follow;
     }
-
->>>>>>> liz
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
-<<<<<<< HEAD
-    public boolean isFollow() {
-        return follow;
-    }
-
-    public void setFollow(boolean follow) {
-        this.follow = follow;
-=======
     public Date getReleaseTime() {
         return releaseTime;
     }
-
     public void setReleaseTime(Date releaseTime) {
         this.releaseTime = releaseTime;
->>>>>>> liz
-    }
 
+    }
     public String getContent() {
         return content;
     }
-
     public void setContent(String content) {
         this.content = content;
     }
-<<<<<<< HEAD
-
-    public Date getReleaseTime() {
-        return ReleaseTime;
-    }
-
-    public void setReleaseTime(Date releaseTime) {
-        ReleaseTime = releaseTime;
-    }
-    //get和set方法到此处为止
-
-=======
     //get和set方法到此为止
->>>>>>> liz
+
 }
--- app/src/main/java/android/bignerdranch/myapplication/PostsActivity.java
+++ app/src/main/java/android/bignerdranch/myapplication/PostsActivity.java
@@ -2,28 +2,8 @@ package android.bignerdranch.myapplication;
 
 import android.content.Context;
 import android.content.Intent;
-<<<<<<< HEAD
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class PostsActivity extends AppCompatActivity {
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.posts_layout);}//表示启动时将posts_layout作为视图
-
-    public static Intent newIntent(Context packageContext) {
-        Intent intent = new Intent(packageContext, PostsActivity.class);//使用传递来的这个Activity创建一个新的Intent
-                                // （之后多半会使用创建的这个Intent来创建一个新的PostsActivity，用以启动PostsActivity）
-        return intent;
-    }
-=======
-import android.os.Build;
-import android.os.Bundle;
-import android.view.WindowManager;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class PostsActivity extends BaseActivity {
 
@@ -37,5 +17,5 @@ public class PostsActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.posts_layout);
     }
->>>>>>> liz
+
 }
