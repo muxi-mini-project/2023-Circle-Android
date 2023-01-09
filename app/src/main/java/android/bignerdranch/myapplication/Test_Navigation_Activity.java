2845425afe0bb50b44f2b1762d2f999ef5ff61f0
6Serendipity <1738328927@qq.com>
2023-01-09 22:50:41 +0800

    个人信息编辑界面

--- app/src/main/java/android/bignerdranch/myapplication/Test_Navigation_Activity.java
+++ app/src/main/java/android/bignerdranch/myapplication/Test_Navigation_Activity.java
@@ -1,6 +1,7 @@
 package android.bignerdranch.myapplication;
 
 import android.bignerdranch.myapplication.ReminderListRecyclerView.Activity_Reminder_List;
+import android.bignerdranch.myapplication.User_Information_Edit.Activity_User_Information;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Test_Navigation_Activity extends AppCompatActivity {
 
     private Button mReminder_List_Test_Button;
+    private Button mUser_Information_Edit_Test_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,15 @@ public class Test_Navigation_Activity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(Test_Navigation_Activity.this, Activity_Reminder_List.class);
+                startActivity(intent);
+            }
+        });
+
+        mUser_Information_Edit_Test_Button=(Button) findViewById(R.id.user_information_edit_navigation);
+        mUser_Information_Edit_Test_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Test_Navigation_Activity.this, Activity_User_Information.class);
                 startActivity(intent);
             }
         });
